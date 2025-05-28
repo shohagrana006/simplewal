b8a2ac8b430925e0cf02d4f49b28e057ce810ab2
shohagrana006 <shohagrana006@gmail.com>
2025-05-29 00:18:19 +0600

    single file read and write with terminal

--- cmd/simplewal/main.go
+++ cmd/simplewal/main.go
@@ -1,12 +1,50 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
 
 	"github.com/shohagrana006/simplewal/internal/wal"
+	pb "github.com/shohagrana006/simplewal/proto"
 )
 
 func main(){
-	result := wal.Add(2,3)
-	fmt.Printf("hello test build file %v", result)
+	walWriter, err := wal.NewWAL()
+	if err != nil{
+		panic(err)
+	}
+
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Println("Enter key: ")
+	key, _ := reader.ReadString('\n')
+	key = strings.TrimSpace(key)
+
+	fmt.Println("Enter your value: ")
+	value, _ := reader.ReadString('\n')
+	value = strings.TrimSpace(value)
+
+	entry := &pb.Entry{
+		Key: key,
+		Value: value,
+	}
+	
+	if err := walWriter.Write(entry); err != nil{
+		panic(err)
+	}
+	
+
+	entries, err := wal.ReadAll()
+	if err != nil{
+		panic(err)
+	}
+
+	for _, e := range entries{
+		fmt.Printf("Key is: %s & Value is: %s\n", e.Key, e.Value)
+	}
+
+
+
 }
--- internal/wal/wal.go
+++ internal/wal/wal.go
@@ -1,20 +1,92 @@
 package wal
 
 import (
+	"encoding/binary"
+	"io"
+	"os"
+	"path/filepath"
+
+	pb "github.com/shohagrana006/simplewal/proto"
 	"google.golang.org/protobuf/proto"
-	protoapi "github.com/shohagrana006/simplewal/proto"
 )
 
+const (
+	LogDir = "./log"
+)
 
-
-func Add(a,b int) int{
-	return a+b
+type WAL struct {
+	file *os.File
 }
 
-func Wal() ([]byte, error) {
-
-	entry := &protoapi.Entry{
-		Name: "shohag",
+func NewWAL() (*WAL, error){
+	if err := os.MkdirAll(LogDir, 0755); err != nil{
+		return nil, err
 	}
-	return proto.Marshal(entry);
+
+	filename := filepath.Join(LogDir, "wal.log")
+
+	file, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil{
+		return nil, err
+	}
+
+	w := &WAL{file: file}
+	return w, nil
+}
+
+func (w *WAL) Write(entry *pb.Entry) error{
+	data, err := proto.Marshal(entry)
+	if nil !=  err{
+		return err
+	}
+
+	lengthBuf := make([]byte, 4)
+	binary.BigEndian.PutUint32(lengthBuf, uint32(len(data)))
+
+	if _, err := w.file.Write(lengthBuf); err != nil {
+		return err
+	}
+
+	if _, err := w.file.Write(data); err != nil{
+		return err
+	}
+	return nil
+}
+
+func ReadAll() ([]*pb.Entry, error){
+	var entries []*pb.Entry
+	filename := filepath.Join(LogDir, "wal.log")
+
+	file, err := os.Open(filename)
+	if err != nil{
+		return nil, err
+	}
+	defer file.Close()
+
+		for{
+			lengthBuf := make([]byte, 4)
+			_, err := io.ReadFull(file, lengthBuf)
+			if err == io.EOF{
+				break
+			}
+			if err != nil{
+				return nil, err
+			}
+
+			length := binary.BigEndian.Uint32(lengthBuf)
+
+			data := make([]byte, length)
+			_, err = io.ReadFull(file, data)
+			if err != nil{
+				return nil, err
+			}
+
+			var entry pb.Entry
+			if err := proto.Unmarshal(data, &entry); err != nil{
+				return nil, err
+			}
+			entries = append(entries, &entry)
+		}
+
+	return entries, nil
 }
